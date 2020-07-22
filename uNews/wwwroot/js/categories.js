1405413d6035a656177d5296c9792b754d5289da
MaxYakovlev <y.m.s.21@mail.ru>
2020-07-22 14:23:12 +0300

    fix currencies and categories

--- uNews/wwwroot/js/categories.js
+++ uNews/wwwroot/js/categories.js
@@ -4,32 +4,60 @@
     let rollUp = document.getElementById("rollup-categories");
     let unRoll = document.getElementById("unroll-categories");
 
-    categories.style.height = "50px";
-    unRoll.style.display = "block";
+    if (document.documentElement.clientWidth > 1182) {
+        categories.style.height = "2680px";
+    }
+    else if (document.documentElement.clientWidth <= 1182 && document.documentElement.clientWidth > 974) {
+        categories.style.height = "2820px";
+    }
+    else if (document.documentElement.clientWidth <= 974) {
+        categories.style.height = "45px";
+        rollUp.style.display = "none";
+        unRoll.style.display = "block";
+    }
 
     categoriesTitle.addEventListener("click", (e) => {
-        e.preventDefault();
+
+        if (document.documentElement.clientWidth <= 974 && rollUp.style.display !== "block") {
+            categories.style.height = "2590px";
 
             // раскрыть категории
-        if (rollUp.style.display !== "block") {
-            if (document.documentElement.clientWidth <= 320) {
-                categories.style.height = "2590px";
-            }
-            else if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                // код для мобильных устройств
-                categories.style.height = "2580px";
-                unRoll.style.display = "block";
-            }
-
             rollUp.style.display = "block";
             unRoll.style.display = "none";
-        }            
-        else { // свернуть категории
-            categories.style.height = "50px";
+        }
+        else if (document.documentElement.clientWidth <= 974 && unRoll.style.display !== "block") {
+            categories.style.height = "45px";
 
+            // свернуть категории
+            setTimeout(() => {
+                rollUp.style.display = "none";
+                unRoll.style.display = "block";
+            }, 700);
+        }
+
+        e.preventDefault();
+    });
+
+    window.addEventListener("resize", (e) => {
+        e.preventDefault();
+
+        if (document.documentElement.clientWidth > 1182) {
+            categories.style.height = "2680px";
             rollUp.style.display = "none";
-            unRoll.style.display = "block";
+            unRoll.style.display = "none";
+        }
+        else if (document.documentElement.clientWidth <= 1182 && document.documentElement.clientWidth > 974) {
+            categories.style.height = "2820px";
+            rollUp.style.display = "none";
+            unRoll.style.display = "none";
+        }
+        else if (document.documentElement.clientWidth <= 974) {
+            if (categories.style.height !== "45px") {
+                categories.style.height = "2580px";
+                rollUp.style.display = "block";
+                unRoll.style.display = "none";
+            }
         }
     });
-    
+
 });
