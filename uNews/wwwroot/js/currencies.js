7ac0bc3ebed76f762f48643999ed0bb49f29cd5a
MaxYakovlev <y.m.s.21@mail.ru>
2020-07-22 20:49:50 +0300

    fix currencies #4

--- uNews/wwwroot/js/currencies.js
+++ uNews/wwwroot/js/currencies.js
@@ -1,50 +1,19 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
     let currenciesTitle = document.getElementById("currency-title");
-    let currencies = document.getElementById("currencies-container");
     let rollUp = document.getElementById("rollup-currency");
     let unRoll = document.getElementById("unroll-currency");
-
-    if (document.documentElement.clientWidth <= 1182 && document.documentElement.clientWidth > 974)
-        currencies.style.height = "210px";
-    else currencies.style.height = "180px";
-
-    unRoll.style.display = "block";
 
     currenciesTitle.addEventListener("click", (e) => {
         e.preventDefault();
 
         // раскрыть категории
-        if (currencies.style.height === "180px" || currencies.style.height === "210px") {
-            if (document.documentElement.clientWidth <= 1182 && document.documentElement.clientWidth > 974)
-                currencies.style.height = "2620px";
-            else if (document.documentElement.clientWidth <= 974)
-                currencies.style.height = "2270px";
-
+        if (rollUp.style.display !== "block") {
             rollUp.style.display = "block";
             unRoll.style.display = "none";
         }
-        else {  // свернуть категории
-            if (document.documentElement.clientWidth <= 1182 && document.documentElement.clientWidth > 974)
-                currencies.style.height = "210px";
-            else currencies.style.height = "180px";
-
-            setTimeout(() => {
-                rollUp.style.display = "none";
-                unRoll.style.display = "block";
-            }, 600);
-        }
-    });
-
-    window.addEventListener("resize", (e) => {
-        e.preventDefault();
-
-        if (document.documentElement.clientWidth <= 1182 && document.documentElement.clientWidth > 974)
-            currencies.style.height = "210px";
-        else currencies.style.height = "180px";
-
-        setTimeout(() => {
+        else {// свернуть категории
             rollUp.style.display = "none";
             unRoll.style.display = "block";
-        }, 600);
+        }
     });
 });
