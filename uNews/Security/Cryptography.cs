552fea33e9eda5a025583e0b26a07cb31f132b47
MaxYakovlev <y.m.s.21@mail.ru>
2021-01-22 02:54:21 +0300

    hide security code

--- uNews/Security/Cryptography.cs
+++ uNews/Security/Cryptography.cs
@@ -5,15 +5,11 @@ namespace uNews.Security
 {
     public static class Cryptography
     {
-        private readonly static string Key = "!@#my$%^secret*&/key?`~3";
+        private readonly static string Key = ""; // secret key is hide
 
         public static string EncryptPassword(string password)
         {
-            string result = password + Key;
-
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(result);
-
-            return Convert.ToBase64String(passwordBytes);
+            // code is hide
         }
     }
 }
